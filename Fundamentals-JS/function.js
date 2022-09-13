2b96726fa5195b85f08a42a6863cae9ec51f6713
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-14 00:57:53 +0600

    function done

--- Fundamentals-JS/function.js
+++ Fundamentals-JS/function.js
@@ -1,1 +1,31 @@
 // 1.5 function (return, parameter)
+
+// 
+
+// programming example doubleIt {kon number ka double korta (+,-,*,/) sob kora jaba}
+function doubleIt(num){
+    var result = num * 2;
+    console.log(result);
+}
+doubleIt(5); // 10
+doubleIt(100); // 200
+doubleIt(1000); // 2000
+
+// sob gula totel kora sob valu sumation korar joonno 
+function doubleIts(num){
+    var mathResult = num * 2;
+    return mathResult;
+}
+var first = doubleIts(5);
+var secoud = doubleIts(100);
+var thard = doubleIts(1000);
+var total = first + secoud + thard;
+console.log(total); // 2210
+
+// more example return:
+function add(num1, num2, num3){
+    var result = num1 + num2 + num3;
+    return result;
+}
+var sum = add(50, 10, 100);
+console.log(sum); // 160
