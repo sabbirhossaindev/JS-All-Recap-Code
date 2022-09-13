2fd76f1cc40c7958c9362e8b1587de7b1b8bb7fb
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-13 21:53:44 +0600

    variable

--- Fundamentals-JS/variable.js
+++ Fundamentals-JS/variable.js
@@ -1,1 +1,12 @@
-// 1.1 variable (let, const) three type variable.
+// 1.1 variable (var, let, const) three type variable.
+
+// var explore:
+
+// var is a function & global scope. can br Redeclare can br reassign initialized with undefined.
+var js = 'i am learning javaScript'
+console.log(js); //i am learning javaScript
+
+// let explore:
+
+// let is a block scope. can't br Redeclare, can br reassign initialized.
+
