123a521ce9d5b93440a953e5cb6676d80a1be6b2
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-13 22:57:31 +0600

    variable done

---
+++ Fundamentals-JS/shorthand.js
@@ -0,0 +1,43 @@
+// variable declaration 
+var tomatoPrice = 25;
+// variable value update
+tomatoPrice = 30;
+
+// add something to the previous value 
+var price1 = 31;
+var price2 = 10;
+
+// set a new value
+// price1 = 35;
+
+// add some value to the existing value 
+price1 = price1 + 10;
+// console.log(price1);
+
+// addition to the some variable shorthand
+// += means adding some value to the existing value 
+price1 += 10;
+
+price2 = price2 - 5;
+// shorthand -=
+price2 -= 5;
+
+// shorthand *=
+price1 *= 5;
+// console.log(price1);
+
+// /=
+price1 /= 5;
+console.log(price1);
+
+var age = 20;
+// age = age + 1;
+// ++ means adding 1 to the existing
+age++;
+console.log(age);
+
+var age = 20;
+// age = age - 1;
+// --
+age--;
+console.log(age);
--- Fundamentals-JS/variable.js
+++ Fundamentals-JS/variable.js
@@ -2,11 +2,22 @@
 
 // var explore:
 
-// var is a function & global scope. can br Redeclare can br reassign initialized with undefined.
+// var is a function & global scope. can be Redeclare can be reassign initialized with undefined.
 var js = 'i am learning javaScript'
 console.log(js); //i am learning javaScript
 
 // let explore:
 
-// let is a block scope. can't br Redeclare, can br reassign initialized.
+// let is a block scope. can't be Redeclare, can be reassign initialized.
+let my = 100;
+my = 10;
+console.log(my); // 10
 
+
+// const explore:
+
+// const is a block scope. can't be Redeclare, can't be reassign initialized.
+const isSabbir = 'student';
+const str = isSabbir + ' ' + 'HPI';
+const total = str;
+console.log(total); // student HPI
