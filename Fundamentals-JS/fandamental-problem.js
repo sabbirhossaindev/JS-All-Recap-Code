2ae7b3126a1260a205b6d3378996d88eb6e9a8e8
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-13 21:34:25 +0600

    JS-All-Recap-Code

---
+++ Fundamentals-JS/fandamental-problem.js
@@ -0,0 +1,10 @@
+/*
+1. Fundamentals:
+    1.1 variable (let, const) three type variable.
+    1.2 condition (>, <, >=, <=, ==, !=, ===, !==) &&, || , if-else....
+    1.3 array (declare, length, index, push, pop, indexOf, includes)...
+    1.4 loop (for loop, while loop)....
+    1.5 function (return, parameter)
+    1.6 object (property, including array, object)
+---------------------------- Fundamentals -------------------------------
+*/
---
+++ english-problem-js.js
@@ -0,0 +1,70 @@
+/*
+--------------  javaScript Fundamentals By Md: Sabbir Hossain ----------------
+1. Fundamentals:
+    1.1 variable (let, const) three type variable.
+    1.2 condition (>, <, >=, <=, ==, !=, ===, !==) &&, || , if-else....
+    1.3 array (declare, length, index, push, pop, indexOf, includes)...
+    1.4 loop (for loop, while loop)....
+    1.5 function (return, parameter)
+    1.6 object (property, including array, object)
+---------------------------- Fundamentals -------------------------------
+1.5 basic data type:
+    1.5.1 string কি জিনিস।  স্ট্রিং এর উপরে কি লুপ চালানো যায়।
+     length, includes, indexOf, toUpperCase, toLowerCase, জানতেই হবে।
+      subString, concat জানলে ভালো।  string কি mutable নাকি immutable 
+    1.5.2 number সম্পর্কে জানতেই হবে। integer, float কোনগুলা।
+    1.5.3 true false কখন ইউজ করে
+    1.5.4 truthy falsy
+    1.5.5 null এবং undefined কি জিনিস।
+------------------------------- basic data type ------------------------------
+2. ES6:
+    2.1 template string ``
+    2.2 spread (...)
+        2.2.1 copy an array then add a new element to an array.
+        2.2.2 use filter to remove an element from an array.
+    2.3 arrow function(const myArrow = () => console.log();)
+        2.3.1 no parameter()
+        2.3.2 single parameter.
+        2.3.3 multiple parameter.
+        2.3.4 multiline
+    2.4 destructuring (object destructuring, array destructuring)
+    2.5 object shorthand 
+    // object declaration shorthand
+        const name = 'sabbir';
+        const age = 20;
+        const objSabbir = {
+            name: name,
+            age: age
+        }
+        // shortcut
+        const obj1 = { name, age };
+        console.log(obj1); // {name: 'sabbir', age: 20}
+    2.6 function parameter default value (a, b=7)
+    2.7 Math use with:  min, max, ceil, floor, abs, round, random. apply ???
+    2.8 Date declare
+    2.9 optional chaining ?.
+---------------------------------- ES6 --------------------------------
+3. Browser API:
+    3.1 local Storage , session storage
+    3.2 location API
+    3.3 history API
+    3.fetch
+----------------------------- Browser API -------------------------------
+4 Dragging:
+    4.1 console tab
+    4.2 source tab
+    4.3 application tab
+    4.4 Network tab
+    4.5 Elements tab 
+5. Other :
+    5.1 array; (map, forEach, filter, find)
+    5.2 ternary operator (isActive ? showUser() : hideUser();)
+    5.3 logical && logical ||
+    5.4 JSON(stringify, parse)
+    5.5 ++, --, +, +'', +=, -=      active = !active 
+    5.6 Object.keys, Object.values
+    5.7 event loop, closure,
+    5.8 this keyWord
+------------------------------------- Other -------------------------------
+------------------------------- Allah Hafiz -------------------------------
+*/
