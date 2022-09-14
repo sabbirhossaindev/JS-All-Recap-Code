9a126a010ee71ce7db638fc7218e5d78cba1917c
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-14 11:54:12 +0600

    functional all done

---
+++ Fundamentals-JS/switch.js
@@ -0,0 +1,35 @@
+// if else er moto just aita diya fiext answer poya jay
+// [default use kora hoya karon right ans na thakla oita swo korba]
+
+num = 100;
+switch(num){
+    case 1000:
+        console.log("i am a sabbir");
+        break;
+    case 100:
+        console.log("i am student");
+        break;
+    case 50:
+    case 40:    
+        console.log("i am either 50 or 40");
+        break;
+    default:
+        console.log("I don't know who are you");
+} // i am student
+
+//more example
+num = 10;
+switch(num){
+    case 1000:
+        console.log("i am a sabbir");
+        break;
+    case 100:
+        console.log("i am 100");
+        break;
+    case 50:
+    case 40:    
+        console.log("i am either 50 or 40");
+        break;
+    default:
+        console.log("I don't know who are you");
+} // I don't know who are you
