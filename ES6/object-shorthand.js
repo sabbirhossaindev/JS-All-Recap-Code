cb9554f56428d7eeba2182bf2d3db4f50bce880f
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-15 17:50:24 +0600

    object shorthand

---
+++ ES6/object-shorthand.js
@@ -0,0 +1,12 @@
+// 2.5 object shorthand
+
+    // object declaration shorthand
+    const name = 'sabbir';
+    const age = 20;
+    const objSabbir = {
+        name: name,
+        age: age
+    }
+    // shortcut
+    const obj1 = { name, age };
+    console.log(obj1); // {name: 'sabbir', age: 20}
