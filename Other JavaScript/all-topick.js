fef9cb4d0a97ed7241347edc6055792d14135a46
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-15 11:01:30 +0600

    other js

---
+++ Other JavaScript/all-topick.js
@@ -0,0 +1,12 @@
+/*
+5. Other :
+    5.1 array; (map, forEach, filter, find)
+    5.2 ternary operator (isActive ? showUser() : hideUser();)
+    5.3 logical && logical ||
+    5.4 JSON(stringify, parse)
+    5.5 ++, --, +, +'', +=, -=      active = !active 
+    5.6 Object.keys, Object.values
+    5.7 event loop, closure,
+    5.8 this keyWord
+------------------------------------- Other -------------------------------
+*/
