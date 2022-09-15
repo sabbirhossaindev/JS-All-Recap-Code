c707b72269aed1ae087add4808b5009a569aa741
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-15 10:55:15 +0600

    data type

---
+++ basic data type/all-problems.js
@@ -0,0 +1,11 @@
+/*
+1.5 basic data type:
+    1.5.1 string কি জিনিস।  স্ট্রিং এর উপরে কি লুপ চালানো যায়।
+     length, includes, indexOf, toUpperCase, toLowerCase, জানতেই হবে।
+      subString, concat জানলে ভালো।  string কি mutable নাকি immutable 
+    1.5.2 number সম্পর্কে জানতেই হবে। integer, float কোনগুলা।
+    1.5.3 true false কখন ইউজ করে
+    1.5.4 truthy falsy
+    1.5.5 null এবং undefined কি জিনিস।
+------------------------------- basic data type ------------------------------
+*/
