581509e21e9d4823f57407ac707455157cb06a45
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-16 09:48:26 +0600

    math done

--- ES6/math-method.js
+++ ES6/math-method.js
@@ -1,1 +1,39 @@
 // 2.7 Math use with:  min, max, ceil, floor, abs, round, random. apply ???
+// The JavaScript Math object allows you to perform mathematical tasks on numbers.
+
+/*
+Math.E        // returns Euler's number
+Math.PI       // returns PI
+Math.SQRT2    // returns the square root of 2
+Math.SQRT1_2  // returns the square root of 1/2
+Math.LN2      // returns the natural logarithm of 2
+Math.LN10     // returns the natural logarithm of 10
+Math.LOG2E    // returns base 2 logarithm of E
+Math.LOG10E   // returns base 10 logarithm of E
+*/
+
+/*
+Math.round(x)	Returns x rounded to its nearest integer
+Math.ceil(x)	Returns x rounded up to its nearest integer
+Math.floor(x)	Returns x rounded down to its nearest integer
+Math.trunc(x)	Returns the integer part of x (new in ES6)
+*/
+
+console.log(Math.ceil(4.9)); //5
+console.log(Math.ceil(4.1)); //5
+
+console.log(Math.floor(4.9)); //4
+
+console.log(Math.trunc(4.5)); // 4
+
+console.log(Math.min(0, 150, 30, 20, -8, -200)); // -200
+
+console.log(Math.max(0, 150, 30, 20, -8, -200)); // 150
+
+console.log(Math.random() * 100); // 30.971916750271046
+
+// Returns a random integer from 0 to 9:
+console.log(Math.floor(Math.random() * 10)); // 6
+
+// Returns a random integer from 1 to 10:
+console.log(Math.floor(Math.random() * 10) + 1); // 8
--- basic data type/all-problems.js
+++ basic data type/all-problems.js
@@ -3,6 +3,7 @@
     1.5.1 string কি জিনিস।  স্ট্রিং এর উপরে কি লুপ চালানো যায়।
      length, includes, indexOf, toUpperCase, toLowerCase, জানতেই হবে।
       subString, concat জানলে ভালো।  string কি mutable নাকি immutable 
+      https://www.youtube.com/watch?v=Po7yLhlBudg&list=PLHiZ4m8vCp9OkrURufHpGUUTBjJhO9Ghy&index=17
     1.5.2 number সম্পর্কে জানতেই হবে। integer, float কোনগুলা।
     1.5.3 true false কখন ইউজ করে
     1.5.4 truthy falsy
