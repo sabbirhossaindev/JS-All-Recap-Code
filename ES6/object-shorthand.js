0349b33f0b55c5eefdd9c0bfa431a4e779e3e442
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-17 19:54:02 +0600

    es6

--- ES6/object-shorthand.js
+++ ES6/object-shorthand.js
@@ -10,4 +10,24 @@
     
     // shortcut
     const obj1 = { name, age };
-    console.log(obj1);  // {name: 'sabbir', age: 20}
+console.log(obj1);  // {name: 'sabbir', age: 20}
+    
+// object declaration
+var student = {
+    id: 477470,
+    name: 'sabbir',
+    class: '5th semester',
+    marks: 3.86
+}
+console.log(student.class); // 5th semester
+
+// 
+var mobile = {
+    brand: 'tacno',
+    peice: 1500,
+    storage: '128gb',
+    camera: '16/48 MP'
+}
+console.log(mobile); // { brand: 'tacno', peice: 1500, storage: '128gb', camera: '16/48 MP' }
+mobile.storage = '256gb'; 
+console.log(mobile); // { brand: 'tacno', peice: 1500, storage: '256gb', camera: '16/48 MP' }
