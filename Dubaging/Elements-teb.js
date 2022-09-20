4474c41c616e721723dd9c94c7ee7a7d05dad07d
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-20 17:50:05 +0600

    all tool done

---
+++ Dubaging/Elements-teb.js
@@ -0,0 +1,3 @@
+/*
+The Elements tab lets you see all the web page elements that were rendered from the HTML and CSS that was retrieved by Safari. The Elements tab is composed of three components: the DOM (Document Object Model) inspector pane, the CSS (Cascading Style Sheet) inspector pane, and the bottom toolbar / breadcrumb ribbon.
+*/
---
+++ Dubaging/network-tab.js
@@ -0,0 +1,3 @@
+/*
+Click the Networking tab in the Windows Task Manager. This window provides a real-time graphical view of the current network activities of the computer. The information provided is for all network adapters installed on the computer.
+*/
