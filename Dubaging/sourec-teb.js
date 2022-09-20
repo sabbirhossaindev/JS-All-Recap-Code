1467b8ac169faa9a346d03d0a8c279f5acc946b4
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-20 17:43:34 +0600

    source teb

---
+++ Dubaging/sourec-teb.js
@@ -0,0 +1,8 @@
+/*
+These are scripts which you can run, author and execute within the Sources panel of Chrome DevTools.
+...
+You can do this by running one of the following commands:
+Command + Control + C on macOs.
+Control + Shift + C on Windows and Linux.
+Just right click anywhere on the browser and click Inspect.
+*/
