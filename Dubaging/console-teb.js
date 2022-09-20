a20dfd278c77b81d1364816173cebb0594ab6b27
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-20 17:40:42 +0600

    console teb

---
+++ Dubaging/console-teb.js
@@ -0,0 +1,11 @@
+/*
+Summary: in this tutorial, you will learn how to open the Console tab of web development tools to view the messages.
+
+Web development tools allow you to test and debug the JavaScript code. Web development tools are often called devtools.
+
+Modern web browsers such as Google Chrome, Firefox, Edge, Safari, and Opera provide the devtools as built-in features.
+
+Generally, devtools allow you to work with a variety of web technologies such as HTML, CSS, DOM, and JavaScript.
+
+In this tutorial, you will learn how to open the Console tab of the devtools to view messages output by JavaScript
+*/
