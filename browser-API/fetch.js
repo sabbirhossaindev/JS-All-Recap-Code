7e15840d664fa74ed82d37d47e3a7c4ede28ffc3
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-22 23:10:57 +0600

    foreach or map done

--- browser-API/fetch.js
+++ browser-API/fetch.js
@@ -4,3 +4,9 @@ fetch('url')
     .then(res => res.json())
     .then(data => console.log(data));
 */
+const loadPhoneDetails = async id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data)
+};
